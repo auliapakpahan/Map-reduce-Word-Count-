6c389d4d6018a457bbf6e4293e090c9a4c22a0ea
auliapakpahan <55121424+auliapakpahan@users.noreply.github.com>
2021-02-28 16:13:06 +0700

    final commit

--- reduce.py
+++ reduce.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
+"""reducer.py"""
 
+from operator import itemgetter
 import sys
-from collections import Counter
 
-def run_reduce(f):
-    cnt = Counter()
-    for line in f:
-        data = line.rstrip()
-        cnt[data] += 1
-    for word, count in cnt.items():
-        print(word, ':',count)
+current_word = None
+current_count = 0
+word = None
 
-if __name__ == '__main__':
-    run_reduce(sys.stdin)
+
+for line in sys.stdin:
+    
+    line = line.strip()
+    
+    word, count = line.split('\t', 1)
+    
+    try:
+        count = int(count)
+    except ValueError:
+       
+        continue
+
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            
+            print ('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+
+if current_word == word:
+    print ('%s\t%s' % (current_word, current_count))
